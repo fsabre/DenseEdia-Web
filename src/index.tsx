fb53285555f7523673a4fed97422c18e09f0cf1d
Fabien Sabre <fabien.sabre@gmail.com>
2021-11-11 00:16:36 +0100

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { initializeIcons } from '@fluentui/react/lib/Icons';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+initializeIcons();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <p>DenseEdia Web</p>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
